65c29e9702da9a25b83b4dc454c5ad131974739a
AnshAcharya <anshacharya265@gmail.com>
2023-11-03 21:58:36 +0530

    oj-question 2 started

---
+++ ojq1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int check_unique(char str[], int start, int end)
+{
+    
+}
+
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    char str[n + 1];
+    scanf("%s", str);
+    //printf("%s\n", str);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+               
+        }
+    }
+    return 0;
+}
