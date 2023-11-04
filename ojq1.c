22f93b88076eda2a26a0150ee3c44a65879665b3
AnshAcharya <anshacharya265@gmail.com>
2023-11-04 23:54:48 +0530

    progress on q1

--- ojq1.c
+++ ojq1.c
@@ -2,22 +2,37 @@
 
 int check_unique(char str[], int start, int end)
 {
-    
+    int flag = 1;
+    for(int i = start; i < end; i++)
+    {
+        if(str[i] == str[i + 1])
+        {
+            continue;
+        }
+        else
+        {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
 }
 
 int main()
 {
-    int n, k;
+    int n, k, cnt = 0;
     scanf("%d %d", &n, &k);
     char str[n + 1];
     scanf("%s", str);
     //printf("%s\n", str);
     for (int i = 0; i < n; i++)
     {
-        for (int j = i; j < n; j++)
-        {
-               
-        }
+        if(check_unique(str, i, i + k) == 1);
+            {
+                cnt++;
+                i += k;
+            }
     }
+    printf("%d\n", cnt);
     return 0;
 }
